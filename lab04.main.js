4a1d6095ec824a342720a813df9bd1d514c89797
MarkSturmer <Mark.Sturmer@Rocketmail.com>
2020-12-09 16:06:04 +0000

    Lab 5

---
+++ lab04.main.js
@@ -0,0 +1,54 @@
+// Constant options
+const options = {
+  url: 'https://dev69821.service-now.com/',
+  username: 'admin',
+  password: 'Tonkabutt1!',
+  serviceNowTable: 'change_request'
+};
+
+// Import built-in Node.js package path.
+const path = require('path');
+
+/**
+ * Import the ServiceNowConnector class from local Node.js module connector.js.
+ *   and assign it to constant ServiceNowConnector.
+ * When importing local modules, IAP requires an absolute file reference.
+ * Built-in module path's join method constructs the absolute filename.
+ */
+const ServiceNowConnector = require(path.join(__dirname, './connector.js'));
+
+/**
+ * @function mainOnObject
+ * @description Instantiates an object from the imported ServiceNowConnector class
+ *   and tests the object's get and post methods.
+ */
+function mainOnObject() {
+  // Instantiate an object from class ServiceNowConnector.
+  const connector = new ServiceNowConnector(options);
+
+/**
+ * @callback iapCallback
+ * @description A [callback function]{@link
+ *   https://developer.mozilla.org/en-US/docs/Glossary/Callback_function}
+ *   is a function passed into another function as an argument, which is
+ *   then invoked inside the outer function to complete some kind of
+ *   routine or action.
+ *
+ * @param {*} responseData - When no errors are caught, return data as a
+ *   single argument to callback function.
+ * @param {error} [errorMessage] - If an error is caught, return error
+ *   message in optional second argument to callback function.
+ * @param {iapCallback} callback - Callback a function.
+ */
+
+
+  // Test the object's get and post methods.
+  // You must write the arguments for get and post.
+  console.error('main.js start');
+  connector.get(callback);
+  /////connector.post();
+
+}
+
+// Call mainOnObject to run it.
+mainOnObject();
