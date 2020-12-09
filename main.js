e37e5e2d7174c9bdb7e92f7eed7a05a77172ae5b
MarkSturmer <Mark.Sturmer@Rocketmail.com>
2020-12-09 21:40:18 +0000

    Lab 6

--- main.js
+++ main.js
@@ -83,6 +83,60 @@ class ServiceNowAdapter extends EventEmitter {
     this.healthcheck();
   }
 
+/**
+ * @memberof ServiceNowAdapter
+ * @method healthcheck
+ * @summary Check ServiceNow Health
+ * @description Verifies external system is available and healthy.
+ *   Calls method emitOnline if external system is available.
+ *
+ * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
+ *   that handles the response.
+ */
+healthcheck(callback) {
+ this.getRecord((result, error) => {
+   /**
+    * For this lab, complete the if else conditional
+    * statements that check if an error exists
+    * or the instance was hibernating. You must write
+    * the blocks for each branch.
+    */
+   if (error) {
+     /**
+      * Write this block.
+      * If an error was returned, we need to emit OFFLINE.
+      * Log the returned error using IAP's global log object
+      * at an error severity. In the log message, record
+      * this.id so an administrator will know which ServiceNow
+      * adapter instance wrote the log message in case more
+      * than one instance is configured.
+      * If an optional IAP callback function was passed to
+      * healthcheck(), execute it passing the error seen as an argument
+      * for the callback's errorMessage parameter.
+      */
+      this.emitOffline();
+   } else {
+     /**
+      * Write this block.
+      * If no runtime problems were detected, emit ONLINE.
+      * Log an appropriate message using IAP's global log object
+      * at a debug severity.
+      * If an optional IAP callback function was passed to
+      * healthcheck(), execute it passing this function's result
+      * parameter as an argument for the callback function's
+      * responseData parameter.
+      */
+      this.emitOnline();
+   }
+ });
+}
+
+
+
+
+
+
+
   /**
    * @memberof ServiceNowAdapter
    * @method healthcheck
@@ -93,11 +147,18 @@ class ServiceNowAdapter extends EventEmitter {
    * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
    *   that handles the response.
    */
+   /*
   healthcheck(callback) {
     // We will build this method in a later lab. For now, it will emulate
     // a healthy integration by emmitting ONLINE.
     this.emitOnline();
   }
+  */
+
+
+
+
+
 
   /**
    * @memberof ServiceNowAdapter
@@ -108,7 +169,7 @@ class ServiceNowAdapter extends EventEmitter {
    */
   emitOffline() {
     this.emitStatus('OFFLINE');
-    log.warn('ServiceNow: Instance is unavailable.');
+    log.error('ServiceNow: Instance is unavailable.');
   }
 
   /**
@@ -120,7 +181,7 @@ class ServiceNowAdapter extends EventEmitter {
    */
   emitOnline() {
     this.emitStatus('ONLINE');
-    log.info('ServiceNow: Instance is available.');
+    log.debug('ServiceNow: Instance is available.');
   }
 
   /**
@@ -188,94 +249,3 @@ class ServiceNowAdapter extends EventEmitter {
 }
 
 module.exports = ServiceNowAdapter;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
-/////////////////////////////// OLD MAIN.JS //////////////////////////////////
-/*
-// Constant options
-const options = {
-  url: 'https://dev69821.service-now.com/',
-  username: 'admin',
-  password: 'Tonkabutt1!',
-  serviceNowTable: 'change_request'
-};
-
-// Import built-in Node.js package path.
-const path = require('path');
-
-/**
- * Import the ServiceNowConnector class from local Node.js module connector.js.
- *   and assign it to constant ServiceNowConnector.
- * When importing local modules, IAP requires an absolute file reference.
- * Built-in module path's join method constructs the absolute filename.
- */
-/* 
-const ServiceNowConnector = require(path.join(__dirname, './connector.js'));
-
-/**
- * @function mainOnObject
- * @description Instantiates an object from the imported ServiceNowConnector class
- *   and tests the object's get and post methods.
- */
- /* 
-function mainOnObject() {
-  // Instantiate an object from class ServiceNowConnector.
-  const connector = new ServiceNowConnector(options);
-
-/**
- * @callback iapCallback
- * @description A [callback function]{@link
- *   https://developer.mozilla.org/en-US/docs/Glossary/Callback_function}
- *   is a function passed into another function as an argument, which is
- *   then invoked inside the outer function to complete some kind of
- *   routine or action.
- *
- * @param {*} responseData - When no errors are caught, return data as a
- *   single argument to callback function.
- * @param {error} [errorMessage] - If an error is caught, return error
- *   message in optional second argument to callback function.
- * @param {iapCallback} callback - Callback a function.
- */
-
-
-  // Test the object's get and post methods.
-  /* 
-  connector.get((data, error) => {
-    if (error) {
-      console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
-    }
-    console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`)
-  });
-  connector.post((data, error) => {
-    if (error) {
-      console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
-    }
-    console.log(`\nResponse returned from POST request:\n${JSON.stringify(data)}`)
-  });
-
-}
-
-// Call mainOnObject to run it.
-mainOnObject();
-*/
